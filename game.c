bdfbb168f7762bcde28b9aafd72f30afd07f6a8b
Valentin Gaillard <valentin.gaillard@etu.u-bordeaux.fr>
2016-02-17 16:29:04 +0100

    fichiers de base non modifiés

---
+++ game.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "game.h"
+
+struct game {
+  int x;
+  int y;
+  int nbMoves;
+
+
+game new_game_hr (int nb_pieces, piece *pieces){
+  game zgeg;
+  return zgeg;
+}
+
+void delete_game (game g) {
+
+}
+
+void copy_game (cgame src, game dst) {
+}
+
+int game_nb_pieces(cgame g) {
+  return 69;
+}
+
+cpiece game_piece(cgame g, int piece_num) {
+  cpiece meboul;
+  return meboul; 
+}
+
+bool game_over_hr(cgame g) {
+  return true;
+}
---
+++ piece.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "piece.h"
+
+struct piece {
+  int x, y;
+  int shape;
+  const int color; //later
+  bool orientation;};
+
+piece new_piece_rh (int x, int y, bool small, bool horizontal) {
+  piece CacaPella;
+  return CacaPella;
+}
+
+void delete_piece (piece p) {
+}
+
+void copy_piece (cpiece src, piece dst) {
+
+}
+
+void move_piece (piece p, dir d, int distance) {
+
+}
+
+
+bool intersect(cpiece p1, cpiece p2) {
+  return 69;
+}
+
+int get_x(cpiece p) {
+  return 69;
+}
+
+int get_y(cpiece p) {
+  return 69;
+}
+
+
+int get_height(cpiece p) {
+  return 69;
+}
+
+int get_width(cpiece p) {
+  return 69;
+}
+
+bool is_horizontal(cpiece p) {
+  return 69;
+}
+
