9304931c3f48054d7f2496b461707ae2f14d2d9c
Ali Cherifi <Ali Cherifi>
2016-02-21 09:45:42 +0100

    Implémentation des fonctions delete_game et copy_game. Début d'implémentation de new_game_hr

--- game.c
+++ game.c
@@ -2,19 +2,27 @@
 #include <stdlib.h>
 
 #include "game.h"
+#include "piece.h"
 
-struct game {
-  int x;
-  int y;
-  int nbMoves;
+#define DIMENSION 6
 
 
-game new_game_hr (int nb_pieces, piece *pieces){
-  game game;
-  return game;
+game new_game_hr (int nb_pieces, piece *pieces){ //On suppose que dans le taleau de piece, la piece à i = 0 est la piece a faire sortir pour gagner (voiture rouge)
+    game  current = malloc(sizeof(struct game_s)); //Allocation de la structure
+    current->nbMoves = 0;
+
+    current->grid = malloc(DIMENSION * sizeof(int*));       // Création de la grille de jeu
+    for (int i = 0; i<DIMENSION; i++){
+        current->grid[i] = malloc(DIMENSION * sizeof(int));
+    }
+    return current;
 }
-
 void delete_game (game g) {
+    for (int i = 0; i<DIMENSION; i++){ //free des sous tableaux
+        free(g->grid[i]);
+    }
+    free(g->grid); //free du tableau restant
+    free(g);    //free de la structure
 
 }
 
@@ -27,7 +35,7 @@ int game_nb_pieces(cgame g) {
 
 cpiece game_piece(cgame g, int piece_num) {
   cpiece piece;
-  return piece; 
+  return piece;
 }
 
 bool game_over_hr(cgame g) {
