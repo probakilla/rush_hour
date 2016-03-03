b284a6b4a8eb0ef9d1ac60c0be3e53eba3a86e20
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2016-03-03 10:14:49 +0100

    corrections de problèmes de compilation de rush_hour.c

--- rush_hour.c
+++ rush_hour.c
@@ -17,25 +17,28 @@ int main (void) {
   piece* pieces3 = malloc (sizeof(struct piece_s) * 4);
 
   // FIRST GAME
-  piece1[0] = new_piece_rh (1, 0, false, true);
-  piece1[1] = new_piece_rh (2, 4, false, false);
-  piece1[2] = new_piece_rh (1, 5, true, false);
-  piece1[3] = new_piece_rh (4, 3, true, false);
-  piece1[4] = new_piece_rh (4, 5, true, true);
+  pieces1[0] = new_piece_rh (3, 0, true,  true);
+  pieces1[1] = new_piece_rh (2, 4, false, false);
+  pieces1[2] = new_piece_rh (1, 5, true, false);
+  pieces1[3] = new_piece_rh (4, 3, true, false);
+  pieces1[4] = new_piece_rh (4, 5, true, true);
+  pieces1[5] = new_piece_rh (1, 0, false, true);
 
   // SECOND GAME
-  piece2[0] = new_piece_rh (2, 2, false, true);
-  piece2[1] = new_piece_rh (1, 2, false, false);
-  piece2[2] = new_piece_rh (3, 4, true, false);
-  piece2[3] = new_piece_rh (5, 1, true, false);
-  piece2[4] = new_piece_rh (1, 5, true, true);
+  pieces1[0] = new_piece_rh (3, 0, true,  true);
+  pieces2[1] = new_piece_rh (2, 2, false, true);
+  pieces2[2] = new_piece_rh (1, 2, false, false);
+  pieces2[3] = new_piece_rh (3, 4, true, false);
+  pieces2[4] = new_piece_rh (5, 1, true, false);
+  pieces2[5] = new_piece_rh (1, 5, true, true);
 
   // THIRD GAME
-  piece3[0] = new_piece_rh (3, 4, false, true);
-  piece3[1] = new_piece_rh (5, 3, false, false);
-  piece3[2] = new_piece_rh (3, 2, true, false);
-  piece3[3] = new_piece_rh (4, 1, true, false);
-  piece3[4] = new_piece_rh (0, 5, true, true);
+  pieces1[0] = new_piece_rh (3, 0, true,  true);
+  pieces3[1] = new_piece_rh (3, 4, false, true);
+  pieces3[2] = new_piece_rh (5, 3, false, false);
+  pieces3[3] = new_piece_rh (3, 2, true, false);
+  pieces3[4] = new_piece_rh (4, 1, true, false);
+  pieces3[5] = new_piece_rh (0, 5, true, true);
 
   // ARRAY OF DIFFERENT GAMES
   piece** games = malloc (sizeof(struct piece_s*) * 3);
@@ -43,10 +46,11 @@ int main (void) {
   games[1] = pieces2;
   games[2] = pieces3;
 
-  cgame game = games[rand() % 2];
+  cgame game = new_game_hr (6, games[rand() % 2]);
     
-  while (!game_over_hr(g)){
+  while (!game_over_hr(game)){
 
   
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
+  }
 }
