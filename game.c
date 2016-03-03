4ecc5ad3afb27d4db80dd136037aced5e94e63a9
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2016-03-03 11:37:06 +0100

    corrections de déplacements de structures

--- game.c
+++ game.c
@@ -7,41 +7,21 @@
  
 #define DIMENSION 6
 
-/**
- * @struct game_s
- * @brief The state of the game is described by the position of the pieces (i.e. vehicles) on the board and the number of moves since the beginning of the game.
- *
- * The game board is a 6*6 grid. The lower left corner has coordinates (0,0).
- * The piece number 0 represents the "red car" that needs to be moved away from the parking.
- * Its initial position is (0,3).
- * A winning game (state) is when the red car touches the right border of the game board (i.e. when its position is (4,3)).
- *
- * cgame is a pointer toward a constant game.
- * That means that it is not possible to modify the game thanks to this pointer.
- * See also: http://www.geeksforgeeks.org/const-qualifier-in-c/
- * See also this more technical discussion: http://stackoverflow.com/questions/8504411/typedef-pointer-const-weirdness
- *
- **/
-
 struct game_s{
   int **grid;
   int nbMoves;
   piece *pieces;
 };
 
-typedef struct game_s* game;
-typedef const struct game_s* cgame;
-
-
 game new_game_hr (int nb_pieces, piece *pieces){ //On suppose que dans le tableau de piece, la piece à i = 0 est la piece a faire sortir pour gagner (voiture rouge)
   game  current = malloc(sizeof(struct game_s)); //Allocation de la structure
-  current->nbMoves = 0;
+  current.nbMoves = 0;
    
-  current->grid = malloc(DIMENSION * sizeof(int*));       // Création de la grille de jeu
+  current.grid = malloc(DIMENSION * sizeof(int*));       // Création de la grille de jeu
   for (int i = 0; i<DIMENSION; i++){
-    current->grid[i] = malloc(DIMENSION * sizeof(int));
+    current.grid[i] = malloc(DIMENSION * sizeof(int));
     for (int j = 1; j<DIMENSION; ++j){
-      current->grid[i][j] = -1;
+      current.grid[i][j] = -1;
     }
   }
   
@@ -49,12 +29,12 @@ game new_game_hr (int nb_pieces, piece *pieces){ //On suppose que dans le tablea
   for (int i = 0; i< (sizeof(pieces) / sizeof(struct piece_s)); i++){    //Parcours du tableau de pieces NE FONCTIONNE PAS POUR UNE PIECE de coordonnées (0,x)
     if (is_horizontal(*pieces)){
       for (int j = get_x(*pieces); j <= get_width(*pieces); j++){ //j sert de point de départ pour placer la piece
-	current->grid[get_y(*pieces)][j] = nomPiece;     //abscisse fixe ordonnée variable
+	current.grid[get_y(*pieces)][j] = nomPiece;     //abscisse fixe ordonnée variable
       }
     }
     else{
       for(int j = get_y(*pieces); j <= get_height(*pieces); j++){
-	current->grid[j][get_x(*pieces)] = nomPiece;
+	current.grid[j][get_x(*pieces)] = nomPiece;
       }
     }
     ++nomPiece;
@@ -64,17 +44,17 @@ game new_game_hr (int nb_pieces, piece *pieces){ //On suppose que dans le tablea
 }
 void delete_game (game g) {
   for (int i = 0; i<DIMENSION; i++){ //free des sous tableaux
-    free(g->grid[i]);
+    free(g.grid[i]);
   }
-  free(g->grid); //free du tableau restant
+  free(g.grid); //free du tableau restant
   free(g);    //free de la structure
 }
  
 void copy_game (cgame src, game dst) {
-  dst->nbMoves = src ->nbMoves;
+  dst.nbMoves = src .nbMoves;
   for (int i =0; i<DIMENSION; i++){
     for (int j = 0; j<DIMENSION; j++){
-      dst->grid[i][j] = src->grid[i][j];
+      dst.grid[i][j] = src.grid[i][j];
     }
   }
 }
@@ -83,8 +63,8 @@ int game_nb_pieces(cgame g) {
   int max = 0;                         // Le nombre de pièces du tableau est le nombre maximum qu'on trouvera dans la grille +1 (en comptant la pièce 0)
   for (int i =0; i<DIMENSION; i++)     // Parcours du tableau
     for (int j = 0; j<DIMENSION; j++)
-      if (g->grid[i][j] > max)
-	max = g->grid[i][j];          // calcul du max
+      if (g.grid[i][j] > max)
+	max = g.grid[i][j];          // calcul du max
   
   return max + 1;
 }
@@ -93,15 +73,15 @@ cpiece game_piece(cgame g, int piece_num) {
   cpiece res;
   for (int i =0; i<DIMENSION; i++)     // Parcours du tableau
     for (int j = 0; j<DIMENSION; j++)
-      if (g->grid[i][j] == piece_num)
-	for (int k = 0;k < (sizeof(g->pieces) / sizeof(struct piece_s)); ++k)
-	  if (i == g->pieces[k]-> y && j == g->pieces[k]-> x)
-	    res = g->pieces[k];
+      if (g.grid[i][j] == piece_num)
+	for (int k = 0;k < (sizeof(g.pieces) / sizeof(struct piece_s)); ++k)
+	  if (i == g.pieces[k]. y && j == g.pieces[k]. x)
+	    res = g.pieces[k];
   return res;
 }
  
 bool game_over_hr(cgame g) {
-  return g->grid[3][4] == 0;
+  return g.grid[3][4] == 0;
 }
  
 bool play_move(game g, int piece_num, dir d, int distance) {
@@ -109,73 +89,73 @@ bool play_move(game g, int piece_num, dir d, int distance) {
   piece moveCp = move;
   int taille = 0;
   
-  if (moveCp->small == true)
+  if (moveCp.small == true)
     taille = 2;
   else
     taille = 3;
    
-  if (move->horizontal){ //LEFT or RIGHT
+  if (move.horizontal){ //LEFT or RIGHT
     
-    if ( d == 1 && move->x - distance >= 0 ){ //LEFT
-      moveCp->x -= distance;
+    if ( d == 1 && move.x - distance >= 0 ){ //LEFT
+      moveCp.x -= distance;
       for (int i = 0; i<DIMENSION; i++){
 	for (int j = 0; j<DIMENSION; j++){
-	  if ( g->grid[i][j] != -1){
-	    if (intersect(game_piece(g,g->grid[i][j]), moveCp))
+	  if ( g.grid[i][j] != -1){
+	    if (intersect(game_piece(g,g.grid[i][j]), moveCp))
 	      return false;
 	  }
 	}
       }
     }
-    move->x -= distance;
+    move.x -= distance;
     return true;
      
        
-    if ( d == 3 && move->x + distance + taille < DIMENSION ){ //RIGHT
-      moveCp->x += distance;
+    if ( d == 3 && move.x + distance + taille < DIMENSION ){ //RIGHT
+      moveCp.x += distance;
       for (int i = 0; i<DIMENSION; i++){
 	for (int j = 0; j<DIMENSION; j++){
-	  if (g-> grid[i][j] != -1){
-	    if (intersect(game_piece(g,g->grid[i][j]), moveCp))
+	  if (g. grid[i][j] != -1){
+	    if (intersect(game_piece(g,g.grid[i][j]), moveCp))
 	      return false;
 	  }
 	}
       }
     }
-    move->x -= distance;
+    move.x -= distance;
     return true;
   }
    
      
-  if (move->horizontal != true){ //UP or DOWN
+  if (move.horizontal != true){ //UP or DOWN
      
-    if ( d == 2 && move->y + distance < DIMENSION ){ //DOWN
-      moveCp->y += distance;
+    if ( d == 2 && move.y + distance < DIMENSION ){ //DOWN
+      moveCp.y += distance;
       for (int i = 0; i<DIMENSION; i++){
 	for (int j = 0; j<DIMENSION; j++){
-	  if ( g->grid[i][j] != -1){
-	    if (intersect(game_piece(g,g->grid[i][j]), moveCp))
+	  if ( g.grid[i][j] != -1){
+	    if (intersect(game_piece(g,g.grid[i][j]), moveCp))
 	      return false;
 	  }
 	}
       }
     }
-    move->y += distance;
+    move.y += distance;
     return true;
      
        
-    if ( d == 4 && move->y - distance + taille >= 0 ){ //UP
-      moveCp->y -= distance;
+    if ( d == 4 && move.y - distance + taille >= 0 ){ //UP
+      moveCp.y -= distance;
       for (int i = 0; i<DIMENSION; i++){
 	for (int j = 0; j<DIMENSION; j++){
-	  if ( g->grid[i][j] != -1){
-	    if (intersect(game_piece(g,g->grid[i][j]), moveCp))
+	  if ( g.grid[i][j] != -1){
+	    if (intersect(game_piece(g,g.grid[i][j]), moveCp))
 	      return false;
 	  }
 	}
       }
     }
-    move->y -= distance;
+    move.y -= distance;
     return true;
   }
 }
@@ -183,5 +163,5 @@ bool play_move(game g, int piece_num, dir d, int distance) {
  
  
 int game_nb_moves(cgame g) {
-  return g->nbMoves;
+  return g.nbMoves;
 }
--- piece.c
+++ piece.c
@@ -3,19 +3,7 @@
 
 #include "piece.h"
 
-/**
- * @struct piece_s
- * @brief A piece represents a vehicle of the game rush hour placed on the board.
- *
- * A piece is characterized by its position, its shape and its ability to move vertically or horizontally.
- * piece is a pointer to the structure that describes a vehicle on the board.
- *
- * cpiece is a pointer toward a constant piece.
- * This means that it is not possible to modify the piece through to this pointer.
- * See also: http://www.geeksforgeeks.org/const-qualifier-in-c/
- * See also this more technical discussion: http://stackoverflow.com/questions/8504411/typedef-pointer-const-weirdness
- *
- */
+
 struct piece_s {
   int x, y;
   bool small;
@@ -24,8 +12,7 @@ struct piece_s {
 };
 
 
-typedef struct piece_s* piece;
-typedef const struct piece_s* cpiece;
+
 
 
 piece new_piece_rh (int x, int y, bool small, bool horizontal) {
