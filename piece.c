9b1d2e1c042e776e5533d0981147c58400ad4631
Ali <acherifi@etu.u-bordeaux.fr>
2016-03-03 11:30:08 +0100

    Reduction de la complexite en temps de intersect. Correction de l'indentation et de la syntaxe des variables. Redaction des commentaires avec Valentin.

--- piece.c
+++ piece.c
@@ -14,18 +14,17 @@ piece new_piece_rh (int x, int y, bool small, bool horizontal) {
   return new;
 }
 
-void delete_piece (piece p) { // pas sûr
-    p->x = -1;
-    p->y = -1;
-    free(p); //libère la mémoire de la pièce
+void delete_piece (piece p) { 
+  p->x = -1;
+  p->y = -1;
+  free(p);
 }
 
 void copy_piece (cpiece src, piece dst) {
-    dst->x = src->x;
-    dst->y = src->y;
-    dst->small = src->small;
-    //dst->color = src->color;
-    dst->horizontal = src->horizontal;
+  dst->x = src->x;
+  dst->y = src->y;
+  dst->small = src->small;
+  dst->horizontal = src->horizontal;
 }
 
 void move_piece (piece p, dir d, int distance) {
@@ -46,47 +45,48 @@ void move_piece (piece p, dir d, int distance) {
 
 bool intersect(cpiece p1, cpiece p2) {
   
-    if ( p1->horizontal == p2->horizontal ){  //Verifie si les pieces sont horizontales et ont des ordonnées différentes
-        if ( p1->horizontal == true ){
-            if ( p1->y != p2->y ){
-                return false;
-            }
-        }
+  if ( p1->horizontal == p2->horizontal ){  //Check if pieces are horizontal and have differents y
+    if ( p1->horizontal == true ){
+      if ( p1->y != p2->y ){
+	return false;
+      }
     }
+  }
     
-if ( p1->horizontal == p2->horizontal ) //Verifie si les pieces sont verticales et ont des abscisses différentes
-        if (p1->horizontal == false )
-            if ( p1->x != p2->x )
-                return false;
+  if ( p1->horizontal == p2->horizontal ) //Check if pieces are vertical and have differents x
+    if (p1->horizontal == false )
+      if ( p1->x != p2->x )
+	return false;
         
     
     
-    int tailleP1 = 0;
-    int tailleP2 = 0;
+  int piece1_extends = 0;
+  int piece2_extends= 0;
     
-    if (p1->small == false)
-        tailleP1 = 3;
-    else 
-        tailleP1 = 2;
+  if (p1->small == false)
+    piece1_extends = 2;
+  else 
+   piece1_extends = 1;
     
-    if (p2->small == false)
-        tailleP2 = 3;
-    else 
-        tailleP2 = 2;
+  if (p2->small == false)
+    piece2_extends= 2;
+  else 
+    piece2_extends= 1;
     
-    for (int i =0; i<tailleP1; ++i){
-        for (int j = 0; j<tailleP2; ++j){
-            if (p1->horizontal == true && p2 ->horizontal == false){
-                if (p1->x+i == p2->x && p1->y == p2->y+j)
-                    return true;
-            }
-            else {
-                if (p1->x == p2->x+j && p1->y+i == p2->y)
-                    return true;
-            }   
-        }
-    }
-    return false;
+  if (p1->horizontal == true && p2 ->horizontal == false){ 
+    if (p2->x >= p1->x && p2->x <= p1->x + piece1_extends) // Test if abscissa of p2 is between the abscissa of p1
+      if (p1->y >= p2->y && p1->y <= p2->y +piece2_extends) // Same test for the y of p1
+	return true;
+  }
+  else {
+    if (p1->x >= p2->x && p1->x <= p2->x + piece2_extends) // Test if abscissa of p1 is between the abscissa of p2 
+      if (p2->y >= p1->y && p2->y <= p1->y + piece1_extends)// Same test for the y of p2 
+	return true;
+     
+  }   
+       
+  
+  return false;
 }
 
 int get_x(cpiece p) {
@@ -99,21 +99,21 @@ int get_y(cpiece p) {
 
 
 int get_height(cpiece p) {
-  	if(p->horizontal==true)
-		return 1;
-	if(p->small==true && p->horizontal==false)
-		return 2;
-	if(p->small==false && p->horizontal==false)
-		return 3;
+  if(p->horizontal==true)
+    return 1;
+  if(p->small==true && p->horizontal==false)
+    return 2;
+  if(p->small==false && p->horizontal==false)
+    return 3;
 }
 
 int get_width(cpiece p) {
-	if(p->horizontal==false)
-		return 1;
-	if(p->small==true && p->horizontal==true)
-		return 2;
-	if(p->small==false && p->horizontal==true)
-		return 3;
+  if(p->horizontal==false)
+    return 1;
+  if(p->small==true && p->horizontal==true)
+    return 2;
+  if(p->small==false && p->horizontal==true)
+    return 3;
 }
 
 bool is_horizontal(cpiece p) {
