9f2947adced7836acee920c297bfdc2f5b6d86b2
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2016-03-17 14:41:38 +0100

    Ajout des fonction de mouvement

--- ane_rouge.c
+++ ane_rouge.c
@@ -32,7 +32,52 @@ int main(void){
 
   game current_game = new_game (DIM_X_AR, DIM_Y_AR, NB_PIECES, pieces);
 
-  display_grid_ar(current_game, pieces, NB_PIECES);
+
+
+  while (!(game_over_ar(current_game))){
+    
+    // COMMANDS
+    printf("Directions commands : 1 = up, 2 = down, 3 = right, 4 = left\n");
+
+    // Variables of command.
+    char car_number [10];
+    int direction;
+    char dir_num [10];
+
+    // Display the game.
+    display_grid_ar(current_game, pieces, NB_PIECES);
+    printf("\n\npiece 0 : x = %d, y = %d\n\n", get_x(game_piece(current_game, 0)), get_y(game_piece(current_game,0)));
+
+    // Check if user choices & test if choices are correct.
+    printf("Select a piece :\n");
+    scanf("%s", car_number);
+
+    printf("Select a move :\n");
+    scanf("%d", &direction);
+
+    printf("How many cases ?\n");
+    scanf("%s", dir_num);
+    
+    // direction convertion
+    dir d = UP;
+    switch (direction){
+    case 1:
+      d = UP;
+      break;
+    case 2 :
+      d = DOWN;
+      break;
+    case 3:
+      d = RIGHT;
+      break;
+    case 4:
+      d = LEFT;
+      break;
+    }
+    play_move (current_game, atoi(car_number), d, atoi(dir_num));
+  }
+  printf("\n\nCongratulation, you  beat this level in %d moves !\n\n", game_nb_moves(current_game));
+  delete_game(current_game);
   
   return EXIT_SUCCESS;
 }
