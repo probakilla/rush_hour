52a64de91c3cd6e58a12453ff7d488ed7fb4905c
Valentin Gaillard <valentin.gaillard@etu.u-bordeaux.fr>
2016-04-05 09:49:52 +0200

    suppression printf

--- src/game.c
+++ src/game.c
@@ -130,7 +130,6 @@ bool play_move(game g, int piece_num, dir d, int distance) {
 		 distance); //Move the real piece, increase the number of moves and free the copy.
       g->nb_moves += distance;
       free(move_copy);
-      printf("%d %d\n", get_x(game_piece(g, piece_num)), get_y(game_piece(g, piece_num)));
       return true;
     }
 
